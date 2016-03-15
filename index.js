15ecb1f1817bc897f88c47bab013fc721e472ae9
Nancy Majors <nmajors@gmail.com>
2016-03-14 19:02:30 -0500

    javascript

---
+++ index.js
@@ -0,0 +1,112 @@
+var valuelessOne;
+var valuelessTwo;
+var valuelessThree;
+var valuelessFour;
+var valuelessFive;
+
+var numberOne = 1;
+var numberTwo = 2;
+var numberThree = 3;
+var numberFour = 4;
+var numberFive = 5;
+
+var stringOne = "First String";
+var stringTwo = "Second String";
+var stringThree = "Third String";
+var stringFour = "Fourth String";
+var stringFive = "Fifth String";
+
+var boolOne = true;
+var boolTwo = true;
+var boolThree = true;
+var boolFour = true;
+var boolFive = true;
+
+var numberSix = 6;
+var numberSeven = 7;
+
+var numberSum = numberSix + numberSeven;
+
+var myAge = true;
+if (myAge = true) {
+  console.log("Truth.");
+} else {
+  console.log("Falsehood.");
+}
+
+for (var i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+var theNumber = 3;
+switch (theNumber) {
+  case 1:
+    console.log("a");
+    break;
+  case 2:
+    console.log("b");
+    break;
+  case 3:
+    console.log("c");
+    break;
+  default:
+    console.log("The number was not found.");
+}
+
+var theTruth = true;
+var loops = 1;
+while (loops <= 10) {
+  console.log(loops + " is less than or equal to 10");
+  loops++;
+}
+if (loops === 11) {
+  theTruth = false;
+  console.log(theTruth);
+}
+
+
+var varOne = 5;
+var varTwo = 6;
+
+if (varOne !== varTwo) {
+  console.log(true);
+}
+
+if (varOne < varTwo) {
+  console.log(true);
+}
+
+if (varTwo < varOne) {
+  console.log(true);
+}
+
+if (varOne === varTwo) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+if (varOne == varTwo) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+if (varTwo < varOne) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+if (varOne > varTwo) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+
+var anotherNumber = false;
+if (anotherNumber === false) {
+  console.log("WAT");
+
+}
